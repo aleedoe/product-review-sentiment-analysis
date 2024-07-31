fc96ead08c634a200b09151677d56d5c57ced272
alie <mushyafaali@gmail.com>
2024-07-31 11:01:57 +0700

    delete comment method

--- tokopedia/insert_to_database.py
+++ tokopedia/insert_to_database.py
@@ -1,14 +1,6 @@
 import json
 
 def read_json_file(file_path):
-    """Reads a JSON file and returns its contents as a Python object.
-
-    Args:
-        file_path: The path to the JSON file.
-
-    Returns:
-        The parsed JSON data as a Python object.
-    """
 
     with open(file_path, 'r') as f:
         data = json.load(f)
