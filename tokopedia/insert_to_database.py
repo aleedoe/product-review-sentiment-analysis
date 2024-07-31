be6c4600be255a9e0bd63e302f05a1c619a9c9c6
alie <mushyafaali@gmail.com>
2024-07-31 11:13:38 +0700

    add method for insert data to database

--- tokopedia/insert_to_database.py
+++ tokopedia/insert_to_database.py
@@ -9,4 +9,50 @@ def read_json_file(file_path):
 # Example usage:
 file_path = 'tokopedia/detail-products.json'
 data = read_json_file(file_path)
-print(data[0])
+# print(data)
+
+import mysql.connector
+
+# Koneksi ke database MySQL
+db = mysql.connector.connect(
+    host="localhost",
+    user="alie",
+    password="kmzway76aa",
+    database="product_review_sentiment_analysis_tokopedia"
+)
+
+# Membuat cursor untuk menjalankan perintah SQL
+cursor = db.cursor()
+
+# Data JSON
+data_list = data
+
+# Ekstraksi data ke dalam list of tuples
+reviews_data = []
+for data in data_list:
+    reviews = data["data"]["productrevGetReviewImage"]["detail"]["reviews"]
+    for review in reviews:
+        feedbackID = review["feedbackID"]
+        reviewText = review["reviewText"]
+        rating = review["rating"]
+        reviewTime = review["reviewTime"]
+        reviews_data.append((feedbackID, reviewText, rating, reviewTime))
+
+# Query untuk bulk insert
+insert_query = """
+INSERT INTO reviews (feedbackID, reviewText, rating, reviewTime)
+VALUES (%s, %s, %s, %s)
+ON DUPLICATE KEY UPDATE
+reviewText = VALUES(reviewText),
+rating = VALUES(rating),
+reviewTime = VALUES(reviewTime)
+"""
+
+# Melakukan bulk insert
+cursor.executemany(insert_query, reviews_data)
+
+# Commit perubahan dan tutup koneksi
+db.commit()
+cursor.close()
+db.close()
+
